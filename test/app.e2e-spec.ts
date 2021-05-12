eadb85e4784da3720cdb704bb113babddcbe738b
hank <hankchiu.tw@gmail.com>
2021-05-12 09:52:46 +0800

    test: implement e2e tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,40 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppTestingModule } from '@app/app-testing.module';
+import { ApiTestingModule } from '@app/api/api-testing.module';
 
-describe('AppController (e2e)', () => {
+describe('ObjectController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [AppTestingModule, ApiTestingModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  it('GET /object/:key', () => {
+    return request(app.getHttpServer()).get('/object/x').expect(200).expect({});
+  });
+
+  it('POST /object', async () => {
+    const expectedKey = 'key1';
+    const expectedValue = 'value1';
+    const res = await request(app.getHttpServer())
+      .post('/object')
+      .send({ [expectedKey]: expectedValue })
+      .expect(201);
+
+    const { key, value, timestamp } = res.body;
+    expect(key).toBe(expectedKey);
+    expect(value).toBe(expectedValue);
+    expect(timestamp).toBeTruthy();
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 });
